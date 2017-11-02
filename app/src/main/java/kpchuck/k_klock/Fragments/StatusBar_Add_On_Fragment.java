43b1d0b5e439a430f919d2a81000d1aae3a3bee4
KpChuck <przestrzelski.com@gmail.com>
2017-11-02 22:23:12 +0000

    Add StatusBar Icon Theming with Custom Colors

--- app/src/main/java/kpchuck/k_klock/Fragments/StatusBar_Add_On_Fragment.java
+++ app/src/main/java/kpchuck/k_klock/Fragments/StatusBar_Add_On_Fragment.java
@@ -1,13 +1,27 @@
 package kpchuck.k_klock.Fragments;
 
+import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import kpchuck.k_klock.Adapters.ColorAdapter;
+import kpchuck.k_klock.Interfaces.BtnClickListener;
+import kpchuck.k_klock.MainActivity;
 import kpchuck.k_klock.R;
 import kpchuck.k_klock.Utils.PrefUtils;
 
@@ -19,9 +33,17 @@ public class StatusBar_Add_On_Fragment extends Fragment {
 
     // TODO: Rename and change types of parameters
     private PrefUtils prefUtils;
+    CardView iconView;
+    private FragmentActivity myContext;
 
     public StatusBar_Add_On_Fragment() {
         // Required empty public constructor
+    }
+    @Override
+    public void onAttach(Activity activity) {
+        myContext=(FragmentActivity) activity;
+
+        super.onAttach(activity);
     }
 
 
@@ -43,6 +65,12 @@ public class StatusBar_Add_On_Fragment extends Fragment {
         Switch leftSwitch = (Switch) v.findViewById(R.id.moveNetworkLeft);
         Switch indicatorSwitch = (Switch) v.findViewById(R.id.networkSignalIndicatorSwitch);
         Switch iconSwitch = (Switch) v.findViewById(R.id.colorIcons);
+        //Setup the add icons cardview
+        Button showIncluded = v.findViewById(R.id.includedIconsButton);
+        Button addColors = v.findViewById(R.id.addIconButton);
+        addColors.setOnClickListener(addColorListener);
+        this.iconView = v.findViewById(R.id.iconColorCardView);
+        showIncluded.setOnClickListener(showColorListener);
         //Initialize the click listeners
         amSwitch.setOnClickListener(amClick);
         lockSwitch.setOnClickListener(lockClick);
@@ -85,7 +113,28 @@ public class StatusBar_Add_On_Fragment extends Fragment {
         public void onClick(View view) {
             Switch qsSwitch = (Switch) getView().findViewById(R.id.colorIcons);
             prefUtils.setSwitchPrefs(qsSwitch, "iconPref");
+            if (qsSwitch.isChecked()) iconView.setVisibility(View.VISIBLE);
+            else iconView.setVisibility(View.GONE);
         }
+    };
+
+    View.OnClickListener addColorListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            InputAlertDialogFragment dialogFragment = new InputAlertDialogFragment();
+            dialogFragment.Instantiate(getString(R.string.add_color_name_title), getString(R.string.add_color_name_hint),
+                    getString(R.string.add_color_value_hint), false, "icons",
+                    false, view);
+            dialogFragment.show(myContext.getSupportFragmentManager(), "klock");
+        }
+    };
+
+    View.OnClickListener showColorListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            ((MainActivity) getActivity()).ShowIncluded(view);
+        }
+
     };
 
 }
