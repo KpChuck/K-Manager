a9a623824b0b3271d526045a12769f10b1ed3540
KpChuck <przestrzelski.com@gmail.com>
2017-11-25 13:45:33 +0000

    Make the help section look better

--- app/src/main/java/kpchuck/k_klock/Activities/InformationWebViewActivity.java
+++ app/src/main/java/kpchuck/k_klock/Activities/InformationWebViewActivity.java
@@ -1,18 +1,36 @@
-package kpchuck.k_klock;
+package kpchuck.k_klock.Activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import kpchuck.k_klock.MainActivity;
+import kpchuck.k_klock.R;
 
 public class InformationWebViewActivity extends AppCompatActivity {
 
+    String slash ="/";
+    String webPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +40,9 @@ public class InformationWebViewActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+       // this.webPath = getApplicationContext().getFilesDir().getPath() + "html";
+        //this.webPath = Environment.getExternalStorageDirectory() + "/K-Manager/html";
+
         int data = 0;
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -29,17 +50,34 @@ public class InformationWebViewActivity extends AppCompatActivity {
         if(bundle != null){
             data = bundle.getInt("value");
         }
-        WebView aboutWebView = (WebView) findViewById(R.id.webViewInfo);
+        WebView aboutWebView = findViewById(R.id.webViewInfo);
         aboutWebView.setBackgroundColor(Color.WHITE);
+        aboutWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+
 
         if (data== 1) {
-            aboutWebView.loadData(styleHtml(this, R.string.general_faq), "text/html", "UTF-8");
+
+            aboutWebView.loadUrl("file:///android_asset/html/index.html");
+            //aboutWebView.loadData(styleHtml(this, R.string.general_faq), "text/html", "UTF-8");
         }else if(data== 2){
-            aboutWebView.loadData(styleHtml(this, R.string.qs_bg), "text/html", "UTF-8");
+            aboutWebView.loadUrl("file:///anroid_asset/html/qsbg.html");
+            //aboutWebView.loadData(styleHtml(this, R.string.qs_bg), "text/html", "UTF-8");
         }else if(data == 3) {
-            aboutWebView.loadData(styleHtml(this, R.string.left_network_indicators), "text/html", "UTF-8");
+            aboutWebView.loadUrl("file:///android_asset/html/left.html");
+            //aboutWebView.loadData(styleHtml(this, R.string.left_network_indicators), "text/html", "UTF-8");
         }else if(data == 4){
-            aboutWebView.loadData(styleHtml(this, R.string.about_other_roms), "text/html", "UTF-8");
+            aboutWebView.loadUrl("file:///android_asset/html/otherroms.html");
+            //aboutWebView.loadData(styleHtml(this, R.string.about_other_roms), "text/html", "UTF-8");
         }else{
             finish();
             Intent i = new Intent(this, MainActivity.class);
@@ -47,6 +85,10 @@ public class InformationWebViewActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    public void shortToast (String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
     public static String styleHtml(Context context, @StringRes int resourceId) {
