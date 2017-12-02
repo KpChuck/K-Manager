2153196af6145162a367d8280bda11e02cb48aa1
KpChuck <przestrzelski.com@gmail.com>
2017-12-02 18:09:50 +0000

    Cleanup the ApkBuilder Class

--- app/src/main/java/kpchuck/k_klock/apkBuilder.java
+++ app/src/main/java/kpchuck/k_klock/apkBuilder.java
@@ -7,6 +7,7 @@ import org.zeroturnaround.zip.ZipUtil;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
@@ -28,44 +29,23 @@ import kpchuck.k_klock.Utils.FileHelper;
 import kpchuck.k_klock.Utils.PrefUtils;
 import kpchuck.k_klock.Utils.QsBgUtil;
 
-public class apkBuilder  extends android.os.AsyncTask<String, String, String> {
+public class apkBuilder  extends AsyncTask<String, String, String> {
 
     String slash = "/";
     String rootFolder = Environment.getExternalStorageDirectory() + slash + "K-Klock";
 
-
-
-    java.io.FilenameFilter fileNameFilter = new java.io.FilenameFilter() {
-
-@Override
-public boolean accept(File dir, String name) {
-        if(name.lastIndexOf('.')>0) {
-
-            // get last index for '.' char
-            int lastIndex = name.lastIndexOf('.');
-
-            // get extension
-            String str = name.substring(lastIndex);
-
-            // match path name extension
-                if(str.equals(".zip")) {
-                     return true;
-                }
-            }
-
-            return false;
-        }
-        };
     private Context context;
     private RelativeLayout relativeLayout;
     private TextView tv;
     private ScrollView frameLayout;
+    FileHelper fileHelper;
 
     public apkBuilder(Context context, RelativeLayout relativeLayout, TextView tv, ScrollView frameLayout){
         this.context=context;
         this.relativeLayout=relativeLayout;
         this.tv=tv;
         this.frameLayout=frameLayout;
+        this.fileHelper = new FileHelper();
 
     }
 
@@ -95,7 +75,7 @@ public boolean accept(File dir, String name) {
                 mergerFolder.mkdirs();
 
                 File ha = new File(rootFolder + slash + "temp" + slash);
-                String[] tempFolder = ha.list(fileNameFilter);
+                String[] tempFolder = ha.list(fileHelper.ZIP);
                 for (String s : tempFolder) {
                     File explosion = new File(rootFolder + slash + "temp" + slash + s);
                     String cool = new String(rootFolder + slash + "temp" + slash + s);
@@ -113,29 +93,32 @@ public boolean accept(File dir, String name) {
                             File pathFile = new File(path);
                             if (pathFile.isDirectory()) {
                                 FileUtils.copyDirectoryToDirectory(pathFile, mergerFolder);
-                            } else {
+                            }
+                            else {
                                 FileUtils.copyFileToDirectory(pathFile, mergerFolder);
                             }
                         } catch (Exception e) {
+                            Log.e("klock", e.getMessage());
                         }
                     }
                 }
 
-
                 findAndCopyXml();
                 new QsBgUtil(context);
+
                 File xmlFolder = new File(rootFolder + slash + "temp3" + slash + "assets");
                 try {
                     FileUtils.copyDirectoryToDirectory(xmlFolder, mergerFolder);
 
                 } catch (IOException e) {
+                    Log.e("klock", e.getMessage());
                 }
 
                 new MoveNetworkIconsLeft(context);
 
-
                 try {
                     createApkFromDir(mergerFolder, new File(rootFolder + slash + "test"), apkVersion[0]);
+
                     File tempFile = new File(rootFolder + slash + "temp");
                     FileUtils.deleteDirectory(tempFile);
                     File temp2File = new File(rootFolder + slash + "temp2");
@@ -145,13 +128,11 @@ public boolean accept(File dir, String name) {
                     File temp3 = new File(rootFolder + slash + "temp3");
                     if (temp3.exists()) FileUtils.deleteDirectory(temp3);
 
-
                 } catch (Exception e) {
+                    Log.e("klock", e.getMessage());
+                }
 
-
-
-
-            }return apkVersion[0];
+                return apkVersion[0];
         }
 
         // This is called from background thread but runs in UI
@@ -165,10 +146,10 @@ public boolean accept(File dir, String name) {
         // This runs in UI when background thread finishes
         @Override
         protected void onPostExecute(String result) {
-                super.onPostExecute(result);
+            super.onPostExecute(result);
 
             String apkVersion = result;
-           File apk = new File(rootFolder + slash + apkVersion);
+            File apk = new File(rootFolder + slash + apkVersion);
 
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
             if (!prefs.getBoolean("installSilently", true) || !Shell.SU.available()) {
@@ -217,88 +198,56 @@ public boolean accept(File dir, String name) {
     }
 
 
-    FilenameFilter fileNameFilterXml = new java.io.FilenameFilter() {
+    public void findAndCopyXml(){
+        File dir = new File(rootFolder);
+        String[] xmlArray = dir.list(fileHelper.XML);
+        makeFolder(rootFolder + slash + "temp3");
+        makeFolder(rootFolder + slash + "temp3" + slash + "assets");
+        makeFolder(rootFolder + slash + "temp3" + slash + "assets" + slash + "overlays");
+        makeFolder(rootFolder + slash + "temp3" + slash + "assets" + slash + "overlays" + slash + "com.android.systemui");
+        File dest_folder = new File(rootFolder + slash + "temp3" + slash + "assets" + slash + "overlays" + slash + "com.android.systemui");
 
-                @Override
-                public boolean accept(File dir, String name) {
-            if(name.lastIndexOf('.')>0) {
+        //Make icon theming folder
+        PrefUtils prefUtils = new PrefUtils(context);
+        boolean themeIcons = prefUtils.getBool("iconPref");
+        if (themeIcons) makeFolder(rootFolder + "/temp3/assets/overlays/com.android.systemui.statusbars");
 
-                    // get last index for '.' char
-                    int lastIndex = name.lastIndexOf('.');
-                    // get extension
-                    String str = name.substring(lastIndex);
-
-
-                //    String typeCheck = name.substring(0, 5);
-                    // match path name extension
-                    if(str.equals(".xml")) {
-                            return true;
-                    }
-            }
-
-            return false;
-                }
-        };
-
-        public void findAndCopyXml(){
-            File dir = new File(rootFolder);
-            String[] xmlArray = dir.list(fileNameFilterXml);
-            makeFolder(rootFolder + slash + "temp3");
-            makeFolder(rootFolder + slash + "temp3" + slash + "assets");
-            makeFolder(rootFolder + slash + "temp3" + slash + "assets" + slash + "overlays");
-            makeFolder(rootFolder + slash + "temp3" + slash + "assets" + slash + "overlays" + slash + "com.android.systemui");
-            File dest_folder = new File(rootFolder + slash + "temp3" + slash + "assets" + slash + "overlays" + slash + "com.android.systemui");
-
-            //Make icon theming folder
-            PrefUtils prefUtils = new PrefUtils(context);
-            boolean themeIcons = prefUtils.getBool("iconPref");
-            if (themeIcons){
-                makeFolder(rootFolder + "/temp3/assets/overlays/com.android.systemui.statusbars");
-            }
-
-
-
-            for(String s : xmlArray){
-                File xml = new File(rootFolder + slash + s);
-                try {
-                    if (themeIcons){
-                        File icon_dest = new File(rootFolder + "/temp3/assets/overlays/com.android.systemui.statusbars/");
-                        if (s.substring(0, 6).equals("type1c")) {
-                            FileUtils.copyFileToDirectory(xml, icon_dest);
-                            xml.delete();
-                        }
-                    }
-                    if (xml.exists()) {
-                        FileUtils.copyFileToDirectory(xml, dest_folder);
+        for(String s : xmlArray){
+            File xml = new File(rootFolder + slash + s);
+            try {
+                if (themeIcons){
+                    File icon_dest = new File(rootFolder + "/temp3/assets/overlays/com.android.systemui.statusbars/");
+                    if (s.substring(0, 6).equals("type1c")) {
+                        FileUtils.copyFileToDirectory(xml, icon_dest);
                         xml.delete();
                     }
-                }catch(IOException e){}
-
                 }
-
-        }
-
-        public void makeFolder(String file){
-            File stuff = new File(file);
-            if(!stuff.exists()){
-                stuff.mkdirs();
+                if (xml.exists()) {
+                    FileUtils.copyFileToDirectory(xml, dest_folder);
+                    xml.delete();
+                }
+            }catch(IOException e){
+                Log.e("klock", e.getMessage());
             }
 
-
         }
 
+    }
 
-        public static void createApkFromDir(File dir, File destFile, String apkVersion) throws Exception {
+    public void makeFolder(String file){
+        File stuff = new File(file);
+        if(!stuff.exists() || !stuff.isDirectory())
+            stuff.mkdirs();
 
-                ZipUtil.pack(dir, destFile);
-                File signedApk = new File(destFile.getParent(), apkVersion);
-                ZipSigner zipSigner = new ZipSigner();
-                zipSigner.setKeymode("auto-testkey");
-                zipSigner.signZip(destFile.getAbsolutePath(), signedApk.getAbsolutePath());
-                Log.d("ZipUtils", "signedZip() : file present -> " + signedApk.exists() + " at " + signedApk.getAbsolutePath());
+    }
 
+    public static void createApkFromDir(File dir, File destFile, String apkVersion) throws Exception {
 
-                }
-
-
-        }
+        ZipUtil.pack(dir, destFile);
+        File signedApk = new File(destFile.getParent(), apkVersion);
+        ZipSigner zipSigner = new ZipSigner();
+        zipSigner.setKeymode("auto-testkey");
+        zipSigner.signZip(destFile.getAbsolutePath(), signedApk.getAbsolutePath());
+        Log.d("ZipUtils", "signedZip() : file present -> " + signedApk.exists() + " at " + signedApk.getAbsolutePath());
+    }
+}
