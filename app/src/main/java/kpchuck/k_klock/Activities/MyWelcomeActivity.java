67606702012138b85e77b0cf14c67cec2fe11a84
KpChuck <przestrzelski.com@gmail.com>
2017-12-12 17:24:00 +0000

    Only show one image in the welcome screen

--- app/src/main/java/kpchuck/k_klock/Activities/MyWelcomeActivity.java
+++ app/src/main/java/kpchuck/k_klock/Activities/MyWelcomeActivity.java
@@ -18,22 +18,23 @@ public class MyWelcomeActivity extends WelcomeActivity {
                 .page(new TitlePage(R.drawable.ic_watch_black_24dp,
                         "K-Klock")
                 )
-                .page(new BasicPage(R.drawable.ic_watch_black_24dp,
+                .page(new BasicPage(R.drawable.transparent,
                         "How to use",
                         "Select your Rom and any other settings in K-Manager. " +
                                 "Then tap the yellow build button to get a K-Klock theme with that configuration! "
                                 )
 
                 )
-                .page(new BasicPage(R.drawable.ic_watch_black_24dp,
+                .page(new BasicPage(R.drawable.transparent,
                         "What the Clock Style option means in Substratum",
                         "\t\tThe first part of the clock style option specifies how the clock should behave.\n" +
                                 "You can choose whether the clock should appear on the lockscreen or not. \n\n" +
                                 "\t\tStock Clock or Dynamic Clock are hidden on the lockscreen without needing to select anything. \n" +
                                 "These two are different because they make the clock dynamic, ie. it changes color against white backgrounds. ")
-                ).page(new BasicPage(R.drawable.ic_watch_black_24dp,
+                ).page(new BasicPage(R.drawable.transparent,
                         "Help and Support",
-                        "If you\'re still confused about anything check the navdrawer! "))
+                        "If you\'re still confused about anything check the navdrawer! \n" +
+                                "You can also check the latest Changelog or check if there\'s an update."))
                 .swipeToDismiss(true)
                 .build();
     }
