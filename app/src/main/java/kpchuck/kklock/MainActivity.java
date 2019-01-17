e00384d5b8f9a282fff4dd7614a69d0e1561b424
KpChuck <przestrzelski.com@gmail.com>
2019-01-17 19:47:06 +0000

    Move donate button down to settings tab

--- app/src/main/java/kpchuck/kklock/MainActivity.java
+++ app/src/main/java/kpchuck/kklock/MainActivity.java
@@ -22,8 +22,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.Pair;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -794,29 +792,6 @@ public class MainActivity extends AppCompatActivity implements ColorPickerDialog
         else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.me/KarolPrzes"));
-            startActivity(browserIntent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
--- app/src/main/java/kpchuck/kklock/xml/StatusBar.java
+++ app/src/main/java/kpchuck/kklock/xml/StatusBar.java
@@ -92,13 +92,13 @@ public class StatusBar extends XmlBase {
         Element statusBarContents = utils.findElementById(workingCopy, idStart + "status_bar_contents");
 
         left = utils.findElementById(workingCopy, idStart + "status_bar_left_side");
-        if (left == null){
-            // Gotta throw everything into a linear layout now, to left of system_icon_area
-            Element divider = utils.findElementById(workingCopy, idStart + "system_icon_area");
-            left = createLinearContainer("left");
-            moveElementsIntoElement(left, utils.getLeftElementsTo(statusBarContents, divider));
-            utils.insertBefore(left, divider);
-        }
+        // Gotta throw everything into a linear layout now, to left of system_icon_area
+        Element divider = utils.findElementById(workingCopy, idStart + "system_icon_area");
+        Element leftSide = createLinearContainer("left");
+        moveElementsIntoElement(leftSide, utils.getLeftElementsTo(statusBarContents, divider));
+        utils.insertBefore(leftSide, divider);
+        if (left == null) left = leftSide;
+
         right = utils.findElementById(workingCopy, idStart + "system_icon_area");
         right.setAttribute("android:gravity", "right");
         right.setAttribute("android:layout_gravity", "right");
